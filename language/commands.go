0aa0e750a7aaf73aad30008669b32729480183ce
Aitor González Fernández <info@aitorgf.com>
2018-11-15 21:05:23 +0100

    Translation to english corrected

--- language/commands.go
+++ language/commands.go
@@ -3,16 +3,16 @@ package language
 import "fmt"
 
 var MessageCommandStart = []translate{
-	translate{ Lang: "en", Text: "Welcome! Subscribe trackers to receive notifications when the registries are opened." },
+	translate{ Lang: "en", Text: "Welcome! Subscribe to trackers in order to receive notifications once the registries are opened." },
 	translate{ Lang: "es", Text: "¡Bienvenido! Suscríbete a trackers para recibir una notificación cuando los registros estén abiertos." },
 }
 
 var MessageCommandUnknown = []translate{
-	translate{ Lang: "en", Text: "Command unknown. Try with /info." },
+	translate{ Lang: "en", Text: "Unknown command . Try with /info." },
 	translate{ Lang: "es", Text: "Comando desconocido. Prueba con /info para obtener ayuda." },
 }
 
 var MessageCommandInfo = []translate{
-	translate{ Lang: "en", Text: fmt.Sprintf("This bot monitor private torrent trackers to notify you when the registries are opened. Can obtain further information on %s.", RepoUrl) },
+	translate{ Lang: "en", Text: fmt.Sprintf("This bot monitors private torrent trackers and will send you notifications once the registries are opened. Further information on %s.", RepoUrl) },
 	translate{ Lang: "es", Text: fmt.Sprintf("Este bot monitoriza trackers torrent privados para avisarte cuando abran los registros. Puedes obtener más información en %s.", RepoUrl) },
 }
--- language/phrases.go
+++ language/phrases.go
@@ -3,32 +3,32 @@ package language
 import "fmt"
 
 var PhraseChooseSubscription = []translate{
-	translate{ Lang: "en", Text: "What do you want to subscribe?" },
+	translate{ Lang: "en", Text: "What do you want to subscribe to?" },
 	translate{ Lang: "es", Text: "¿A que deseas suscribirte?" },
 }
 
 var PhraseIDontUnderstand = []translate{
-	translate{ Lang: "en", Text: "Sorry, but I don't understand you" },
+	translate{ Lang: "en", Text: "Sorry, I couldn't understand you" },
 	translate{ Lang: "es", Text: "Lo siento, pero no te he entendido" },
 }
 
 var PhraseInsertLanguage = []translate{
-	translate{ Lang: "en", Text: "What's language do you like subscribe? (Send the ISO code, example: en for English. Set the - symbol before to unsubscribe, example: -en). The available languages are: " },
+	translate{ Lang: "en", Text: "What languages do you like subscribe to? (Send the ISO code, example: en for English. Set before the symbol - to unsubscribe, example: -en). The available languages are:" },
 	translate{ Lang: "es", Text: "¿A que idioma te quieres suscribir? (Envía el ISO code, ejemplo: es para Español. Pon un - delante para borrar la suscripción, ejemplo: -es). Los idiomas disponibles son: " },
 }
 
 var PhraseLanguageSubscribed = []translate{
-	translate{ Lang: "en", Text: "Well! You are now subscribed to the trackers of this language. You can affine more the notifications subscribing you to concrete categories." },
+	translate{ Lang: "en", Text: "Great! You are now subscribed to the trackers for this language. You can refine the notifications by subscribing to more specific categories." },
 	translate{ Lang: "es", Text: "¡Bien! Te has suscrito a los trackers de este idioma. Puedes ajustar más las notificaciones suscribiendote a categorías concretas." },
 }
 
 var PhraseInsertCategory = []translate{
-	translate{ Lang: "en", Text: "What's category do you like subscribe? (Send the name, example: movies. Set the - symbol before to unsubscribe, example: -movies). The available categories are: " },
+	translate{ Lang: "en", Text: "What categories do you like subscribe to? (Send the name, example: movies. Set before the symbol - to unsubscribe, example: -movies). The available categories are:" },
 	translate{ Lang: "es", Text: "¿A que categoría te quieres suscribir? (Envía el nombre, ejemplo: movies. Pon un - delante para borrar la suscripción, ejemplo: -movies). Las categorías disponibles son: " },
 }
 
 var PhraseCategorySubscribed = []translate{
-	translate{ Lang: "en", Text: "Well! You are now subscribed to the trackers of this category. You can affine more the notifications subscribing you to concrete language." },
+	translate{ Lang: "en", Text: "Great! You are now subscribed to the trackers of this category. You can refine the notifications by subscribing to more specific categories." },
 	translate{ Lang: "es", Text: "¡Bien! Ahora estás suscrito a los trackers de esta categoría. Puedes afinar más las notificacions suscribiendote a un lenguaje o lenguajes concretos." },
 }
 
@@ -43,22 +43,22 @@ var PhraseNoSubscriptions = []translate{
 }
 
 var PhraseUserSubscriptionsCategories = []translate{
-	translate{ Lang: "en", Text: "You are subscribed to the categories: %s." },
+	translate{ Lang: "en", Text: "You are currently subscribed to the categories: %s." },
 	translate{ Lang: "es", Text: "Estás suscrito a las siguientes categorías: %s." },
 }
 
 var PhraseUserSubscriptionsLanguages = []translate{
-	translate{ Lang: "en", Text: "You are subscribed to the languages: %s." },
+	translate{ Lang: "en", Text: "You are currently subscribed to the languages: %s." },
 	translate{ Lang: "es", Text: "Estás suscrito a las siguientes idiomas: %s." },
 }
 
 var PhraseCategoryUnrecognized = []translate{
-	translate{ Lang: "en", Text: "I'm sorry, I don't recognize this category." },
+	translate{ Lang: "en", Text: "I'm sorry, I can't recognize this category." },
 	translate{ Lang: "es", Text: "Lo siento, no reconozco esa categoría." },
 }
 
 var PhraseLanguageUnrecognized = []translate{
-	translate{ Lang: "en", Text: "I'm sorry, I don't recognize this language." },
+	translate{ Lang: "en", Text: "I'm sorry, I can't recognize this language." },
 	translate{ Lang: "es", Text: "Lo siento, no reconozco este idioma." },
 }
 
@@ -73,21 +73,21 @@ var PhraseCategoryUnsubscribed = []translate{
 }
 
 var PhraseNoSubscribed = []translate{
-	translate{ Lang: "en", Text: "Ups, looks like we didn't have any of your subscriptions anymore." },
+	translate{ Lang: "en", Text: "Ups, looks like we didn't already have any of your subscriptions." },
 	translate{ Lang: "es", Text: "Ups, parece que ya no teníamos ninguna suscripción tuya." },
 }
 
 var PhraseActionsCanceled = []translate{
-	translate{ Lang: "en", Text: "All actions has cancelled." },
+	translate{ Lang: "en", Text: "All actions cancelled." },
 	translate{ Lang: "es", Text: "Se han cancelado todas las acciones." },
 }
 
 var PhraseTrackerOnline = []translate{
-	translate{ Lang: "en", Text: "Hey! It seems that the %s tracker registrations are open. Try to sign up: %s." },
+	translate{ Lang: "en", Text: "Hey! It seems that the %s tracker registrations are open. Try to signing up: %s." },
 	translate{ Lang: "es", Text: "¡Ey! Parece que los registros del tracker %s estan abiertos. Intenta registrarte: %s." },
 }
 
 var PhraseTrackerMaybeOnline = []translate{
-	translate{ Lang: "en", Text: "Hi, I've detected that the %s tracker could has open registrations. Try to sign up: %s." },
+	translate{ Lang: "en", Text: "Hi, I've detected that the %s tracker could have opened the registrations. Try to signing up: %s." },
 	translate{ Lang: "es", Text: "Hola, he detectado que el tracker %s podría haber abierto los registros. Intenta registrarte: %s." },
 }
